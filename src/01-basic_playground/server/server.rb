229a9b250f4ea6a4daf03e03fe493b5fcd6f4831
Rafa de Castro <rafael@micubiculo.com>
2020-04-11 20:31:57 +0200

    Creating some variance in response times

--- src/01-basic_playground/server/server.rb
+++ src/01-basic_playground/server/server.rb
@@ -7,5 +7,11 @@ require 'sinatra/reloader'
 
 get '/current_time' do
   content_type :json
+  case rand 100
+  when 1
+    sleep 10
+  when 2..10
+    sleep 2
+  end
   { current_time: Time.now.to_s }.to_json
 end
