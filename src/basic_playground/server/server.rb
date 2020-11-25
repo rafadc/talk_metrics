05def2e8da3c6fd575ce6dcf91810a30a32a46d6
Rafa de Castro <rafael@micubiculo.com>
2020-11-25 19:58:57 +0100

    Added another metric so I have an example

--- src/basic_playground/server/server.rb
+++ src/basic_playground/server/server.rb
@@ -5,13 +5,36 @@ require 'json'
 require 'sinatra'
 require 'sinatra/reloader'
 
+require 'prometheus/client'
+
+prometheus = Prometheus::Client.registry
+
+histogram = Prometheus::Client::Histogram.new(
+  :clockio_complex_calculation_seconds, 
+  docstring: 'The time it takes our secret sauce to calculate the timings', 
+  buckets: [0.01, 0.1, 0.3, 0.6, 1, 1.5, 10, 20]
+)
+prometheus.register(histogram)
+
 get '/current_time' do
   content_type :json
-  case rand 100
-  when 1
-    sleep 10
-  when 2..10
-    sleep 2
-  end
+
+  histogram.observe(
+    Benchmark.realtime { 
+      complex_operation 
+    }
+  )
+
   { current_time: Time.now.to_s }.to_json
 end
+
+BEGIN {
+  def complex_operation
+    case rand 100
+    when 1
+      sleep 10
+    when 2..10
+      sleep 2
+    end
+  end
+}
